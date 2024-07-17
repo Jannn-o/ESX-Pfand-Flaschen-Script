7c31e630facbd06d70a701f73c427fd9e25ab6d2
Jannn.o <108473027+Jannn-o@users.noreply.github.com>
2024-07-17 14:39:06 +0200

    shop.lua

--- JN_Simple_Bottle_Deposit/server/shop.lua
+++ JN_Simple_Bottle_Deposit/server/shop.lua
@@ -1,11 +1,31 @@
-RegisterNetEvent("JN_Pfand:getpfand")
-AddEventHandler("JN_Pfand:getpfand", function(bottles)
+RegisterNetEvent("JN_Pfand:sellpfandbar")
+AddEventHandler("JN_Pfand:sellpfandbar", function(bottles)
     local xPlayer = ESX.GetPlayerFromId(source)
     local amount = xPlayer.getInventoryItem(bottles).count
     if amount >= 1 then
         for k, v in pairs(Config.shop) do 
             xPlayer.removeInventoryItem(bottles, amount)
-            xPlayer.addMoney(amount * v.price)
+            money = (amount * v.price)
+            xPlayer.addAccountMoney("money", money)
+            xPlayer.showNotification("Du hast Pfand Im Wert von " .. money .. " Verkauft")
         end
+    else
+        xPlayer.showNotification("Du hast Keine Pfandflaschen")
+    end
+end)
+
+RegisterNetEvent("JN_Pfand:sellpfandbank")
+AddEventHandler("JN_Pfand:sellpfandbank", function(bottles)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local amount = xPlayer.getInventoryItem(bottles).count
+    if amount >= 1 then
+        for k, v in pairs(Config.shop) do 
+            xPlayer.removeInventoryItem(bottles, amount)
+            money = (amount * v.price)
+            xPlayer.addAccountMoney('bank', money)
+            xPlayer.showNotification("Du hast Pfand Im Wert von " .. money .. " Verkauft")
+        end
+    else
+        xPlayer.showNotification("Du hast Keine Pfandflaschen")
     end
 end)
